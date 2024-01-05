3af89c2776cfdcaf14cf3e4fd49cde6468373072
Ömer Bahrioğlu <bahriogluomer@gmail.com>
2024-01-05 20:54:04 +0300

    created project

---
+++ src/components/Login.jsx
@@ -0,0 +1,81 @@
+import { Form, FormGroup, Label, FormFeedback, FormText, Input } from "reactstrap"
+
+export default function Login () {
+    return(<Form>
+        <FormGroup>
+          <Label for="exampleEmail">
+            Input without validation
+          </Label>
+          <Input />
+          <FormFeedback>
+            You will not be able to see this
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">
+            Valid input
+          </Label>
+          <Input valid />
+          <FormFeedback valid>
+            Sweet! that name is available
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">
+            Invalid input
+          </Label>
+          <Input invalid />
+          <FormFeedback>
+            Oh noes! that name is already taken
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">
+            Input without validation
+          </Label>
+          <Input />
+          <FormFeedback tooltip>
+            You will not be able to see this
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup className="position-relative">
+          <Label for="exampleEmail">
+            Valid input
+          </Label>
+          <Input valid />
+          <FormFeedback
+            tooltip
+            valid
+          >
+            Sweet! that name is available
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+        <FormGroup className="position-relative">
+          <Label for="examplePassword">
+            Invalid input
+          </Label>
+          <Input invalid />
+          <FormFeedback tooltip>
+            Oh noes! that name is already taken
+          </FormFeedback>
+          <FormText>
+            Example help text that remains unchanged.
+          </FormText>
+        </FormGroup>
+      </Form>)
+}
